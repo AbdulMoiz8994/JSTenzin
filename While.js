7df85523298dee3366a1fcfe675790671f1ec9ad
AbdulMoiz8994 <moiza8994@gmail.com>
2021-08-21 23:58:16 +0500

    remove the console

---
+++ While.js
@@ -0,0 +1,18 @@
+console.log("Hello World");
+
+
+
+var num=10;
+var i=0;
+while(i < 100){
+    num += i
+    i++
+}
+console.log(num);
+
+
+var Snum=10;
+while(Snum > 1 || Snum === 10){
+    console.log( Snum);
+    Snum--
+}
